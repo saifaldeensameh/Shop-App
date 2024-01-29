f3d56fcbdfc66ae8d9d3134395dd39af9ae93296
SaifAlDeenSamehMahrous <seifeldeensameh@cis.asu.edu.eg>
2024-01-29 21:43:17 +0200

    validation error alert in login and signup page

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,28 +1,40 @@
 const authmodel = require('../models/auth.model')
-
+const validationresult = require('express-validator').validationResult
 exports.getSignup = (req,res,next)=>{
     
-    res.render('signup')
+    res.render('signup',{
+        ValidationErrors: req.flash('ValidationErrors')
+    })
 
 }
 
 exports.postSignup = (req,res,next)=>{
+
+    // return console.log(validationresult(req))
+    if (validationresult(req).isEmpty()){
     authmodel.createNewUser(req.body.username,req.body.email,req.body.password).then(()=>{
         res.redirect('/login')}).catch(err=>res.redirect('/signup'))
-    
-    // res.render('signup')
+    }
+    else{
+        req.flash('ValidationErrors',validationresult(req).array())
+        res.redirect('signup')
+    }
 
 
 }
 
 exports.getLogin = (req,res,next)=>{
+    
     res.render('login',{
-        authError: req.flash('authError')[0]
+        authError: req.flash('authError')[0],
+        loginValidationErrors: req.flash('loginValidationErrors')
     });
 
 }
 
 exports.postLogin = (req,res,next)=>{
+    if (validationresult(req).isEmpty()){
+    
     authmodel.loginuser(req.body.email,req.body.password)
     .then((id)=>{
         req.session.useId = id 
@@ -31,6 +43,11 @@ exports.postLogin = (req,res,next)=>{
         req.flash('authError',error)
         res.redirect('login')
     })
+    }
+    else{
+        req.flash('loginValidationErrors',validationresult(req).array())
+        res.redirect('login')
+    }
 }
 
 exports.logout = (req,res,next) => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
+const check = require('express-validator').check
 
 const authController = require('../controllers/auth.controller')
 
@@ -7,12 +8,26 @@ router.get('/signup',authController.getSignup)
 
 router.post('/signup',
             bodyParser.urlencoded({extended: true}),
+            check('username').not().isEmpty().withMessage("Username can't be Empty"),
+            check('email').not().isEmpty().withMessage("Email can't be Empty")
+            .isEmail().withMessage('Please Enter Correct format of Email xxxx@xxx.xx'),
+            check('password').isLength({min:6}).withMessage('Password Length Should > 6 characters'),
+            check('confirm_password').custom((value,{req})=>{
+                if (value === req.body.password){
+                    return true;
+                }
+                else {
+                    throw "Password Doesn't Match";
+                }
+            }),
             authController.postSignup)
 
 router.get('/login',authController.getLogin)            
 
 router.post('/login',
             bodyParser.urlencoded({extended:true}),
+            check('email').not().isEmpty().withMessage("Email can't be Empty"),
+            check('password').isLength({min:6}).withMessage('Password Length Should > 6 characters'),
             authController.postLogin)
 
 router.all('/logout',authController.logout)            
